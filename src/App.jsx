6b09fe9fbdc79d60b8fed92707b6805a5c329ebd
brenanwanderley <brenanwanderley@gmail.com>
2021-02-12 22:47:33 -0300

    Project 1 - Group 4

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+
+const App = () => (
+  <div className="">
+    <Header title="PITANG 2" />
+    <div className="p-4">
+      <Card title="Home" />
+    </div>
+  </div>
+);
+
+export default App;
